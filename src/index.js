9c2a4803de1f3e1d9808a32216c204f8c47b7eba
ascotttoney <ascotttoney@gmail.com>
2019-06-12 16:08:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#dog-breeds').addEventListener('click', handleChangeColor)
-  // document.getElementById('breed-dropdown').addEventListener('change', handleLetterSelect)
     fetchPictures()
     fetchBreeds()
   }
 )
 
-
-
 function fetchPictures(){
   return fetch("https://dog.ceo/api/breeds/image/random/4")
   .then(function(response){
     return response.json();
-    // console.log("here is the", response.json())
   })
   .then(function(object){
       (addPictures(object));
-      // console.log("Hello!!", object)
-      // console.log(JSON.stringify(object))
     })
   .catch((error)=>console.log("error", error))
 }
-
 
 function addPictures(object){
   const dogs = object.message
@@ -31,7 +24,6 @@ function addPictures(object){
     // console.log(dog)
   })
 }
-
 
 function addNewPicture(image){
   const image_gallery = document.querySelector('#dog-image-container')
@@ -56,24 +48,13 @@ function fetchBreeds(){
   .catch((error)=>console.log("error", error))
 }
 
-
 function getAllBreeds(object){
   const breeds = object.message
-
-  // handleLetterSelect(breeds)
 
   for (const key in breeds){
     addNewBreed(key)
   }
 }
-
-
-// function filterBreeds(object){
-//   const array = []
-  // console.log(object)
-  // return array
-// }
-
 
 function addNewBreed(breed){
   const breed_list = document.querySelector('#dog-breeds')
@@ -85,11 +66,3 @@ function addNewBreed(breed){
 function handleChangeColor(e){
   e.target.style.color = "red"
 }
-
-// function handleLetterSelect(e) {
-//   console.log("target", e.target.value)
-//   console.log("letter select", e)
-  // if (e.target.childNodes.value === "a") {
-  //   console.log("whoa")
-  // }
-// }
